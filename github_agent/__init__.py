c3da8dc8e1953b15fde28f0bc4436b9ef6c5a7e7
Nick Bobrowski <39348559+bonk1t@users.noreply.github.com>
2026-03-04 22:35:01 +0000

    fix: export github mcp debug constants
    - export GITHUB_MCP_URL and GITHUB_MCP_URL_SOURCE from github_agent package
    
    - fix startup import in agency.py for sandbox deployments

--- github_agent/__init__.py
+++ github_agent/__init__.py
@@ -1,5 +1,4 @@
-from .github_agent import github_agent
+from .github_agent import GITHUB_MCP_URL, GITHUB_MCP_URL_SOURCE, github_agent
 
-__all__ = ["github_agent"]
-
+__all__ = ["github_agent", "GITHUB_MCP_URL", "GITHUB_MCP_URL_SOURCE"]
 
